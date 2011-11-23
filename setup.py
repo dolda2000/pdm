1bb46303ee44603cdbdcba9146f20559126f2e0e
Fredrik Tolf <fredrik@dolda2000.com>
2011-11-23 03:49:34 +0100

    Added a distutils script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = "pdm",
+      version = "0.1",
+      description = "Python daemon management library",
+      author = "Fredrik Tolf",
+      author_email = "fredrik@dolda2000.com",
+      # url = "http://www.dolda2000.com/~fredrik/pdm/",
+      packages = ["pdm"],
+      scripts = ["pdm-repl"],
+      license = "GPL-3")
