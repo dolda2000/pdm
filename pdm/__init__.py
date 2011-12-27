f3bcebd5bab4807a59abf7e08b2af402cb17393a
Fredrik Tolf <fredrik@dolda2000.com>
2011-12-27 23:11:01 +0100

    Fixed up documentation a bit.

--- pdm/__init__.py
+++ pdm/__init__.py
@@ -9,7 +9,7 @@ This package contains the following modules:
 
  - srv -- Server module
  - cli -- Client module
- - perf -- Library for implementing object for the PERF protocol
+ - perf -- Library for implementing objects for the PERF protocol
 
 The protocol allows multiple management subprotocols for different
 modes of operation. Currently, the following two management protocols
@@ -19,13 +19,13 @@ are supported.
    accepts arbitrary Python code, executes it in the daemon process,
    and returns its replies, all in text form. The protocol is simple,
    generic, and has few failure modes, but is hardly suitable for
-   programmatic interaction. See the documentation for pdm.srv.repl
-   and pdm.cli.replclient for further details.
+   programmatic interaction. See the documentation for L{pdm.srv.repl}
+   and L{pdm.cli.replclient} for further details.
 
  - The PERF protocol is intended for programmatic interaction with the
    daemon process. Various Python modules may expose objects that
    implement one or several of a few pre-defined interfaces that allow
    for various forms of inspection and management of the program
-   state. See the documentation for pdm.srv.perf and
-   pdm.cli.perfclient for further details.
+   state. See the documentation for L{pdm.srv.perf} and
+   L{pdm.cli.perfclient} for further details.
 """
